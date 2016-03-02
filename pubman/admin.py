c77254d967f6a940fab6664c159b670ab9265b04
Ondra Lengal <ondra.lengal@gmail.com>
2016-03-03 01:48:33 +0800

    models: add more models

--- pubman/admin.py
+++ pubman/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author
+from .models import ConferenceSeries
+from .models import Conference
+from .models import Publication
+from .models import Publisher
+from .models import ProceedingsSeries
+
+admin.site.register(Author)
+admin.site.register(ConferenceSeries)
+admin.site.register(Conference)
+admin.site.register(Publication)
+admin.site.register(Publisher)
+admin.site.register(ProceedingsSeries)
--- pubman/models.py
+++ pubman/models.py
@@ -1,6 +1,54 @@
 from django.db import models
 
 class Author(models.Model):
-    first_name = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=128, null=True)
     last_name = models.CharField(max_length=128)
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=128)
+
+
+class ProceedingsSeries(models.Model):
+    name = models.CharField(max_length=256)
+    abbr = models.CharField(max_length=128, null=True)
+
+
+class ConferenceSeries(models.Model):
+    # name of the conference series
+    name = models.TextField()
+    # abbreviation
+    abbr = models.CharField(max_length=128, null=True)
+
+
+class Conference(models.Model):
+    # series of the conference
+    conf_series = models.ForeignKey(ConferenceSeries, on_delete=models.SET_NULL, null=True)
+    # year of the conference
+    year = models.IntegerField(null=True)
+    # the run number of the conference
+    run_num = models.IntegerField(null=True)
+    # proceedings were published in series ...
+    published_in = models.ForeignKey(ProceedingsSeries, on_delete=models.SET_NULL, null=True)
+    # the volume of a notes series proceedings were published in
+    published_in_vol = models.IntegerField(null=True)
+
+
+class Publication(models.Model):
+    # title of the contribution
+    title = models.TextField()
+    # appeared in conference?
+    conference = models.ForeignKey(Conference, on_delete=models.SET_NULL, null=True)
+    # authors
+    authors = models.ManyToManyField(Author, through="AuthorOf")
+
+
+class AuthorOf(models.Model):
+    # author
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    # publication
+    publication = models.ForeignKey(Publication, on_delete=models.CASCADE)
+    # order of the author
+    order = models.IntegerField(null=True)
+
 
