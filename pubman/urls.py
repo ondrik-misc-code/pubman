99e02a3fd888dd7e6346ce89ed022443e9d06df3
Ondra Lengal <ondra.lengal@gmail.com>
2016-03-05 13:28:25 +0800

    authors/new: add a view

--- pubman/urls.py
+++ pubman/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'pubman'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^authors/new/$', views.NewAuthorView.as_view(), name='new_author'),
 ]
--- pubman/views.py
+++ pubman/views.py
@@ -1,4 +1,18 @@
 from django.http import HttpResponse
+from django.views import generic
+from django.core.urlresolvers import reverse, reverse_lazy
+
+from .models import Author
 
 def index(request):
     return HttpResponse("Hello, world.")
+
+
+class NewAuthorView(generic.CreateView):
+    '''
+    New author input view.
+    '''
+    model = Author
+    fields = ['first_name', 'last_name']
+    template_name = "pubman/author_edit.html"
+    success_url = reverse_lazy('pubman:index')
